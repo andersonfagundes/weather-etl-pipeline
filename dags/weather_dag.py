1b29237b993a69b413d4c3f31317a125e8dd9918
Anderson <anderson.fagundes4@gmail.com>
2026-05-19 21:45:51 +0100

    🔧 fix(airflow): correct weather DAG execution issues

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -1,4 +1,4 @@
-from datetime import datatime, timedelta
+from datetime import datetime, timedelta
 from airflow.decorators import dag, task
 from pathlib import Path
 import sys, os
@@ -14,40 +14,41 @@ env_path = Path(__file__).resolve().parent.parent / 'config' / '.env'
 load_dotenv(env_path)
 
 API_KEY = os.getenv('API_KEY')
-url = f'https://api.openweathermap.org/data/2.5/weather?q=Sao Paulo, BR&units-metric&appid={API_KEY}'
+
+url = f'https://api.openweathermap.org/data/2.5/weather?q=Sao Paulo,BR&units=metric&appid={API_KEY}'
 
 @dag(
-  dag_id='weather_pipeline',
-  default_args={
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'retries': 2,
-    'retry_delay': timedelta(minutes=5)
-  },
-  description='ETL Pipeline - Weather SP',
-  schedule='0 */1 * * * ',
-  start_date=datetime(2026, 2, 7),
-  catchup=False,
-  tags=['weather', 'etl', 'etl pipeline']
+    dag_id='weather_pipeline',
+    default_args={
+        'owner': 'airflow',
+        'depends_on_past': False,
+        'retries': 2,
+        'retry_delay': timedelta(minutes=5)
+    },
+    description='ETL Pipeline - Weather SP',
+    schedule='0 */1 * * *',
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+    tags=['weather', 'etl']
 )
 
 def weather_pipeline():
 
-  @task
-  def extract():
-    extract_weather_data(url)
+    @task
+    def extract():
+        extract_weather_data(url)
 
-  @task
-  def transform():
-    df = data_transformations()
-    df.to_parquet('/opt/airflow/data/temp_data.parquet', index=False)
+    @task
+    def transform():
+        df = data_transformations()
+        df.to_parquet('/opt/airflow/data/temp_data.parquet', index=False)
 
-  @task
-  def load():
-    import pandas as pd
-    df = pd.read_parquet('/opt/airflow/data/temp_data.parquet')
-    load_weather_data('sp_weather', df)
+    @task
+    def load():
+        import pandas as pd
+        df = pd.read_parquet('/opt/airflow/data/temp_data.parquet')
+        load_weather_data('sp_weather', df)
 
-  extract() >> transform() >> load()
+    extract() >> transform() >> load()
 
 weather_pipeline()
